0aed06e5d3e295f361ecd819063b3c88183d16f7
CodeInTheIzzyverse <dev.izzyverse@gmail.com>
2026-08-23 13:59:17 -0500

    Vitest + Playwrite config

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [swc.vite()],
+  test: {
+    globals: true,
+    root: '.',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'lcov'],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
+    },
+  },
+});
